f649ef7e895b83c4c70cc892c61c009fc49240a8
avasilenko <snakevil@mail.ru>
2024-02-05 13:47:29 +0300

    task1_done

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("input M: ");
+        int M = int.Parse(Console.ReadLine()!);
+
+        Console.Write("input N: ");
+        int N = int.Parse(Console.ReadLine()!);
+
+        Console.WriteLine("natural numbers:");
+        Console.Write('\"' + PrintNumbers(M, N) + '\"');
+    }
+
+    static string PrintNumbers(int first, int final)
+    {
+        string numbers = first.ToString();
+
+        if (first != final)
+        {
+            numbers += ", " + PrintNumbers(first + 1, final);
+        }
+
+        return numbers;
+    }
+}
