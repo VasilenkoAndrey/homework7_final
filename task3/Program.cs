bee1d3fa8f10844ac7a5548f7bf0a8da42b2ee12
avasilenko <snakevil@mail.ru>
2024-02-05 14:10:42 +0300

    task3_done

--- task3/Program.cs
+++ task3/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void ReverseArrayPr(int[] arr, int i)
+    {
+        if (i >= 0)
+        {
+            if (i != arr.Length - 1)
+            {
+                Console.Write(", ");
+            }
+            Console.Write(arr[i]);
+            ReverseArrayPr(arr, i - 1);
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        int[] arr = {15, 42, 63, 78, 90};
+        
+        Console.Write("ReverseResult: ");
+        ReverseArrayPr(arr, arr.Length - 1);
+        Console.WriteLine("");
+    }
+}
